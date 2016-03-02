f8253b8734810c849279fb60785f23dc803bc06c
HeKaMe <henrik.valett@hotmail.de>
2016-03-02 11:56:46 +0100

    Update Brick.java

--- items/Brick.java
+++ items/Brick.java
@@ -1,5 +1,29 @@
 package breakout.items;
 
-public class Brick {
+public class Brick extends PhysicsObject {
+
+private byte brickType;
+private static final brickWidth = 2*PixelSizes.PIXELWIDTH;
+private static final brickHeight = PixelSizes.PIXELHEIGHT;
+
+public Brick(double x, double y, byte brickType){
+this.Position = new Vector2D(x,y);
+setBBox(brickWidth,brickHeight);
+this.brickType = brickType;
+}
+
+public setBrickType (byte brickType){
+  this.brickType = brickType;
+}
+
+public getBrickType (){
+  return this.brickType;
+}
+
+@Override
+public void onCollision(PhysicsObejct obj){
+  brickType--;
+}
+
 
 }
