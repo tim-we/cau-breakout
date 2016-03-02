eaf542ff96dbbbcf23c07eb77daabd7dfe5e362a
HeKaMe <henrik.valett@hotmail.de>
2016-03-02 12:07:02 +0100

    Update Paddle.java

--- items/Paddle.java
+++ items/Paddle.java
@@ -1,10 +1,12 @@
 package breakout.items;
 import breakout.assets.*;
 import breakout.physics.*;
-
+import java.awt.Color;
 public class Paddle extends PhysicsObject{
 
 private double paddleWidth = 4*PIXELWIDTH;
+
+private Color color = new Color (142,33,125);
 
 public Paddle(){
   this.Position = new Vector2D(((28/2)-2)*PIXELWIDTH,13*PIXELHEIGHT)
