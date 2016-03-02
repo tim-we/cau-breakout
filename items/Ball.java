52f677a8ee9e0d2ab7bb82f54de5c8b04deba14a
HeKaMe <henrik.valett@hotmail.de>
2016-03-02 11:23:04 +0100

    Update Ball.java

--- items/Ball.java
+++ items/Ball.java
@@ -1,5 +1,28 @@
 package breakout.items;
+import breakout.physics.*;
+import java.awt.Color;
 
-public class Ball {
+public class Ball extends MovingObject {
+  
+  private double width = 25; 
+  private double height = 50;
 
+	private Color color = new Color(255,0,0);
+	
+	public BreakoutBall(Vector2D position) {
+	  this.Position = position;
+	  setBBox(width,height);
+	}
+	
+	public Color getColor() {
+		return this.color;
+	}
+	
+	public void scaleVelocity(double f) {
+		velocity = velocity.scale(f);
+	}
+	
+	
+	
+}
 }
