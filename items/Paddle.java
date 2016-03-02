bed5ee08566fc232efd79345cb977e437be74b1c
HeKaMe <henrik.valett@hotmail.de>
2016-03-02 11:41:37 +0100

    Update Paddle.java

--- items/Paddle.java
+++ items/Paddle.java
@@ -1,5 +1,22 @@
 package breakout.items;
+import breakout.assets.*;
+import breakout.physics.*;
 
-public class Paddle {
+public class Paddle extends PhysicsObject{
+
+private double paddleWidth = 4*PIXELWIDTH;
+
+public Paddle(){
+  this.Position = new Vector2D(((28/2)-2)*PIXELWIDTH,13*PIXELHEIGHT)
+}
+
+public setPosition (double x){
+  Position.setX(x);
+}
+
+
+
+
+
 
 }
