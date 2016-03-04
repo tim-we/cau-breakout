c32e3c54622839f19ca4ca7ad32c54527b009a70
HeKaMe <henrik.valett@hotmail.de>
2016-03-04 20:18:32 +0100

    Level class

---
+++ levels/Level.java
@@ -0,0 +1,15 @@
+package breakout.levels;
+
+public class Level {
+
+	private final String levelstring;
+	
+	public Level(String string){
+		this.levelstring = string;
+	}
+	
+	public String getString(){
+		return this.levelstring;
+	}
+			
+}
--- levels/LevelLoader.java
+++ levels/LevelLoader.java
@@ -6,46 +6,47 @@ import java.util.Random;
 import breakout.main.Model;
 import breakout.assets.BreakoutConstants;
 import breakout.items.Brick;
+import breakout.levels.*;
 
 public class LevelLoader {
-
-	private static final String[] levels = { 
-			"-222-222-11-111-11-0000000-", 
-			"000000000000000000000000000",
-			"111111111111111111111111111", 
-			"222222222222222222222222222", 
-			"012012012012012012012012012",
-			"010101010101010101010101010", 
-			"121212121212121212121212121", 
-			"020202020202020202020202020",
-			"-1-1-1-1-1-1-1-1-1-1-1-1-1-", 
-			"-1--1--1--1--1--1--1--1--1-",
+	
+	private static final Level[] levels = { 
+			new Level("-222-222-11-111-11-0000000-"), 
+			new Level("000000000000000000000000000"),
+			new Level("111111111111111111111111111"), 
+			new	Level("222222222222222222222222222"), 
+			new Level("012012012012012012012012012"),
+			new Level("010101010101010101010101010"), 
+			new	Level("121212121212121212121212121"), 
+			new Level("020202020202020202020202020"),
+			new	Level("-1-1-1-1-1-1-1-1-1-1-1-1-1-"), 
+			new	Level("-1--1--1--1--1--1--1--1--1-"),
 			
 			//CAU Level:
-				  "?2$2$2--$$1--$$$0-0 "
+		new	Level("?2$2$2--$$1--$$$0-0 "
 				+ "?2$---$1$1-$0-0 "
 				+ "?2--1-$$1-$$0-0 "
 				+ "?2--1$1$1-$$0-0 "
 				+ "?2$--1-10$0$0$0 "
-				+ "?2$2$2-$$1-10$0$0$0 "
+				+ "?2$2$2-$$1-10$0$0$0 ")
 	};
 
 	private LevelLoader(double w) {
 	}
-
-	public static String randomLevel() {
+	
+	public static Level randomLevel() {
 		Random rgen = new Random();
 		int lvl = rgen.nextInt(levels.length);
 		return levels[lvl];
 	}
 
-	public static String getLevelByIndex(int i) {
-		if(i<0 || i>=levels.length) { return ""; }
+	public static Level getLevelByIndex(int i) {
+		if(i<0 || i>=levels.length) { return new Level(""); }
 		
 		return levels[i];
 	}
 
-	public static void setBricks(String brickData, Model m) {
+	public static void setBricks(Level brickData, Model m) {
 		// remove all existing bricks
 		m.clearBricks();
 		
@@ -59,10 +60,10 @@ public class LevelLoader {
 		double yPos = BreakoutConstants.BRICK_Y_OFFSET;
 		xPos = BreakoutConstants.BRICK_X_OFFSET;
 
-		for (int i = 0; i < brickData.length(); i++) {
+		for (int i = 0; i < brickData.getString().length(); i++) {
 			byte brickType = -1;
 
-			switch (brickData.charAt(i)) {
+			switch (brickData.getString().charAt(i)) {
 			case '0':
 				brickType = 0;
 				break;
@@ -84,7 +85,6 @@ public class LevelLoader {
 			case ';':
 				yPos += BreakoutConstants.BRICK_Y_OFFSET;
 				break;
-
 			}
 
 			if (xPos >= m.getWidth()) {
@@ -96,7 +96,7 @@ public class LevelLoader {
 				m.addBrick(new Brick(xPos, yPos, (byte) brickType));
 			}
 
-			if (brickData.charAt(i) != '$' && brickData.charAt(i) != '?' && brickData.charAt(i) != ';') {
+			if (brickData.getString().charAt(i) != '$' && brickData.getString().charAt(i) != '?' && brickData.getString().charAt(i) != ';') {
 				xPos += Brick.brickWidth + BreakoutConstants.BRICK_X_OFFSET;
 			}
 
@@ -105,12 +105,12 @@ public class LevelLoader {
 	}
 	
 	public static void loadLevel(int i, Model m) {
-		String level = LevelLoader.getLevelByIndex(i);
+		Level level = LevelLoader.getLevelByIndex(i);
 		LevelLoader.setBricks(level, m);
 	}
 	
 	public static void loadLevel(Model m) {
-		String level = LevelLoader.randomLevel();
+		Level level = LevelLoader.randomLevel();
 		LevelLoader.setBricks(level, m);
 	}
 }
